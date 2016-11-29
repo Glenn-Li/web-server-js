487f83be9c2ef5af773cb3c30c18845ef154e5db
emlyq <emlyq@hotmail.com>
2016-11-29 11:01:46 +0800

    Creat My First Atom plug - A webserver.

---
+++ spec/web-server-js-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WebServerJsView from '../lib/web-server-js-view';
+
+describe('WebServerJsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
